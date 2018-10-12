71d914f8b4db9bcc7926c5b70e949f69f51ff1db
simpsont <simpsont@gilmour.org>
2018-10-12 14:46:25 -0400

    Gary's code

--- student.py
+++ student.py
@@ -108,7 +108,7 @@ class Piggy(pigo.Pigo):
             self.encR(20)
 
     def surprise(self):
-        """creates the coolest move you have ever seen""""
+        """creates the coolest move you have ever seen"""
          for x in range(2):
             self.encF(30)
             self.encL(5)
